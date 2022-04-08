4b3ffe6915062bcef7ba606ca80105281b9a5049
notarnold <95426496+aargullos@users.noreply.github.com>
2022-04-08 14:59:40 +0200

    Create Cuadrado.java

---
+++ Cuadrado.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Cuadrado {
+    private int costat;
+
+    public int getCostat() {
+        return costat;
+    }
+
+    public void setCostat(int costat) {
+        this.costat = costat;
+    }
+}
