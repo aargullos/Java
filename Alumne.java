e897af21bfe855fa0316e8215df37d36f1afbe92
notarnold <95426496+aargullos@users.noreply.github.com>
2022-04-08 15:01:05 +0200

    Create Alumne.java

---
+++ Alumne.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Alumne {
+    public String name, surname;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getSurname(){
+        return surname;
+    }
+
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+}
