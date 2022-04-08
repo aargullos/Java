82b3788f8022a5bab4450059690c9f3d4c79582d
notarnold <95426496+aargullos@users.noreply.github.com>
2022-04-08 15:01:42 +0200

    Create Coches.java

---
+++ Coches.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class coches {
+    public String marca, modelo;
+    public Integer precio;
+
+    public String getmarca(){
+        return marca;
+    }
+
+    public void setmarca(String marca){
+        this.marca = marca;
+    }
+
+    public String getmodelo(){
+        return modelo;
+    }
+
+    public void setmodelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    public Integer getprecio(){
+        return precio;
+    }
+
+    public void setprecio(Integer precio){
+        this.precio = precio;
+    }
+}
